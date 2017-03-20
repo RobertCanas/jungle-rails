31006369e58fddc21fc4e928e27f25740b9ac5a4
Robert Canas <robert_canas@hotmail.com>
2017-03-20 23:11:04 +0000

    Validation specs for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
 
   has_secure_password
   has_many :reviews
-  validates :email, uniqueness: true
+  validates :email, uniqueness: {case_sensitive: false}
   validates :name, :email, presence: true
+  validates :password_digest, presence: true
+  validates :password_digest, length: { minimum: 6 }
 
 end
--- spec/models/user-spec.rb
+++ spec/models/user-spec.rb
@@ -10,5 +10,16 @@ RSpec.describe User, type: :model do
       })
       expect(@user).to be_valid
     end
+
+    it 'should have matching password and password_confirmation' do
+      @user = User.new({
+        name: 'Bobby',
+        email: 'Bobby123@wings.com',
+        password: 'test123',
+        password_confirmation: 'test1234'
+      })
+      @user.save
+      expect(@user.password).to_not eql(@user.password_confirmation)
+    end
   end
 end
