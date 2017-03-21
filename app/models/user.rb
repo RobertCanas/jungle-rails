ab1fddf0142a5f7270360a7337c4f55c953f5b00
Robert Canas <robert_canas@hotmail.com>
2017-03-21 00:41:00 +0000

    Adding new class method and edge cases for validating login scenarios.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,17 @@ class User < ActiveRecord::Base
 
   has_secure_password
   has_many :reviews
-  validates :email, uniqueness: {case_sensitive: false}
+  validates :email, uniqueness: {case_sensitive: false}, case_sensitive: false
   validates :name, :email, presence: true
-  validates :password_digest, presence: true
-  validates :password_digest, length: { minimum: 6 }
+  validates :password_digest, presence: true, length: { minimum: 6 }
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.where("lower(email) = ?", email.strip.downcase).first
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
 
 end
--- spec/models/user-spec.rb
+++ spec/models/user-spec.rb
@@ -22,4 +22,73 @@ RSpec.describe User, type: :model do
       expect(@user.password).to_not eql(@user.password_confirmation)
     end
   end
+
+  describe '.authenticate_with_credentials' do
+
+
+    it 'should be valid if email and password are valid' do
+     @user = User.new({
+        name: 'Billy',
+        email: 'BillyBob@hotmail.com',
+        password: 'BillyBob123',
+        password_confirmation: 'BillyBob123'
+      })
+     @user.save
+      expect(@user.email).to eq('BillyBob@hotmail.com')
+      expect(@user.password).to eq(@user.password_confirmation)
+    end
+
+    it 'should be valid when credentials are valid' do
+     @user = User.new({
+        name: 'Billy',
+        email: 'BillyBob@hotmail.com',
+        password: 'BillyBob123',
+        password_confirmation: 'BillyBob123'
+      })
+     @user.save
+      expect(User.authenticate_with_credentials('BillyBob@hotmail.com', 'BillyBob123')).to eq(@user)
+    end
+
+    it 'should not be valid on wrong password credential' do
+     @user = User.create({
+        name: 'Billy',
+        email: 'BillyBob@hotmail.com',
+        password: 'BillyBob123',
+        password_confirmation: 'BillyBob123'
+      })
+      expect(User.authenticate_with_credentials('BillyBob@hotmail.com', 'lolcats123')).to be_falsey
+    end
+
+    it 'should not be valid on wrong email credential' do
+     @user = User.create({
+        name: 'Billy',
+        email: 'BillyBob@hotmail.com',
+        password: 'BillyBob123',
+        password_confirmation: 'BillyBob123'
+      })
+      expect(User.authenticate_with_credentials('FakeEmail@hotmail.com', 'BillyBob123')).to be_falsey
+    end
+
+    it 'should be valid when credentials are valid even if there is whitespace' do
+     @user = User.create({
+        name: 'Billy',
+        email: 'BillyBob@hotmail.com',
+        password: 'BillyBob123',
+        password_confirmation: 'BillyBob123'
+      })
+      expect(User.authenticate_with_credentials('  Billybob@hotmail.com  ', 'BillyBob123')).to eq(@user)
+    end
+
+    it 'should be valid when credentials are valid even if it is capitalized' do
+     @user = User.create({
+        name: 'Billy',
+        email: 'BillyBob@hotmail.com',
+        password: 'BillyBob123',
+        password_confirmation: 'BillyBob123'
+      })
+      expect(User.authenticate_with_credentials('BilLyBob@hotMail.com', 'BillyBob123')).to eq(@user)
+    end
+
+  end
+
 end
